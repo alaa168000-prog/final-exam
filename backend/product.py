7f40cb316747019b17a3bcafe6f1daf970c4bf2d
Alaa <alaa168000@gmail.com>
2025-12-02 03:15:33 +0300

    Modification of product.py

--- backend/product.py
+++ backend/product.py
@@ -7,24 +7,24 @@ products = [
 ]
 
 print("Product List:")
-for i, product in enumerate(products, start=1):
+
+i = 1
+for product in products:
     print(str(i) + ". " + product["name"] + " - Price: " + str(product["price"]) + " SAR")
+    i += 1
 
 choice_input = input("Enter the number of the product you want to check the price for: ")
 
-if choice_input.isdigit():
+if choice_input in ['1', '2', '3', '4', '5']:
     choice = int(choice_input)
-    if 1 <= choice <= len(products):
-        selected_product = products[choice - 1]
-        price = selected_product["price"]
-        tax = price * 0.15
-        total_price = price + tax
+    selected_product = products[choice - 1]
+    price = selected_product["price"]
+    tax = price * 0.15
+    total_price = price + tax
 
-        print("\nProduct: " + selected_product["name"])
-        print("Price before tax: " + str(price) + " SAR")
-        print("Tax (15%): " + str(round(tax, 2)) + " SAR")
-        print("Total price including tax: " + str(round(total_price, 2)) + " SAR")
-    else:
-        print("The number you entered is invalid. Please enter a number from 1 to 5.")
+    print("\nProduct: " + selected_product["name"])
+    print("Price before tax: " + str(price) + " SAR")
+    print("Tax (15%): " + str(round(tax, 2)) + " SAR")
+    print("Total price including tax: " + str(round(total_price, 2)) + " SAR")
 else:
-    print("Invalid input. Please enter a valid number.")
+    print("Invalid input. Please enter a number from 1 to 5.")
